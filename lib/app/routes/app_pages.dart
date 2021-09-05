f61daa8455ac2518ff59c39f05f54c850bc62723
rainstsam <rainst@126.com>
2021-09-06 01:05:27 +0800

    新建页面

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,20 +1,43 @@
+/*
+ * @Descripttion: 
+ * @version: 0.1.0
+ * @Author: rainstsam
+ * @Date: 2021-09-05 23:21:09
+ * @LastEditors: rainstsam
+ * @LastEditTime: 2021-09-06 00:42:06
+ */
 import 'package:get/get.dart';
 
+import 'package:bluevoice/app/modules/configtask/bindings/configtask_binding.dart';
+import 'package:bluevoice/app/modules/configtask/views/configtask_view.dart';
 import 'package:bluevoice/app/modules/home/bindings/home_binding.dart';
 import 'package:bluevoice/app/modules/home/views/home_view.dart';
+import 'package:bluevoice/app/modules/splash/bindings/splash_binding.dart';
+import 'package:bluevoice/app/modules/splash/views/splash_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+
+    GetPage(
+      name: _Paths.CONFIGTASK,
+      page: () => ConfigtaskView(),
+      binding: ConfigtaskBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashView(),
+      binding: SplashBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 0.1.0
+ * @Author: rainstsam
+ * @Date: 2021-09-05 23:21:09
+ * @LastEditors: rainstsam
+ * @LastEditTime: 2021-09-06 00:41:42
+ */
 part of 'app_pages.dart';
 // DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
 
@@ -5,8 +13,12 @@ abstract class Routes {
   Routes._();
 
   static const HOME = _Paths.HOME;
+  static const CONFIGTASK = _Paths.CONFIGTASK;
+  static const SPLASH = _Paths.SPLASH;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const CONFIGTASK = '/configtask';
+  static const SPLASH = '/splash';
 }
